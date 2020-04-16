46b428b7fd2b625720ed2f5ab7c5427112d3eb0c
DemonEclipSe <48463140+DemonEclipSe@users.noreply.github.com>
2020-04-16 17:00:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
-# I have no use for this file. It is simply a placeholder.
+from distutils.core import setup
+setup(
+  name = 'authgen',
+  packages = ['authgen'],
+  version = '0.4',
+  license='MIT',
+  description = 'Random string gen',
+  author = 'Zakhar Preobrazhensky',
+  author_email = 'cfym.gamer@gmail.com',
+  url = 'https://github.com/DemonEclipSe/authgen',
+  download_url = 'https://github.com/DemonEclipSe/authgen/tree/master/authgen',
+  keywords = ['wrapper', 'api', 'random'],
+  install_requires=['aiohttp', 'string, 'random'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
