e3a386cd069fd8aa9e399095b4d44dd3cd75d4aa
DemonEclipSe <48463140+DemonEclipSe@users.noreply.github.com>
2020-05-31 02:36:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'authgen',
   packages = ['authgen'],
-  version = '0.4.4',
+  version = '0.4.5',
   license='MIT',
   description = 'Random string gen',
   author = 'Zakhar Preobrazhensky',
